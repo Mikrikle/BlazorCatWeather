b7d22fd2730118c973a31890098f7c0fee63adbd
Mihail <mihail.pe@outlook.com>
2022-11-24 19:26:31 +0300

    fix: add json property names

--- BlazorWeather.Web/Dtos/Weather/WeatherBase.cs
+++ BlazorWeather.Web/Dtos/Weather/WeatherBase.cs
@@ -21,7 +21,9 @@ namespace BlazorWeather.Web.Dtos
     {
         public double Temp { get; set; }
         public double FeelsLike { get; set; }
+        [JsonPropertyName("temp_min")]
         public double TempMin { get; set; }
+        [JsonPropertyName("temp_max")]
         public double TempMax { get; set; }
         public int Pressure { get; set; }
         public int Humidity { get; set; }
